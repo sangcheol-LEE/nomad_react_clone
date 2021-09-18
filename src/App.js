f9f3a11e37d364ac3c43c2f6b1fa179d438581e0
이상철 <zlrz6706@gmail.com>
2021-09-18 14:52:01 +0900

    기본 틀 작업 마무리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -7,21 +8,51 @@ class App extends React.Component {
     movies: [] // 후에 많은 요소들로 채울 예정.
   };
 
-  getMovies = async () => {                                                         // async await 을 하는 것은 우리가 기본적으로 JS에게 getMovies 함수에게  
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");    // 조금 시간이 필요하고 우리는 그걸 기다려야한다 라는 것을 의미합니다.
-  }
+  getMovies = async () => {  
+                                                          // async await 을 하는 것은 우리가 기본적으로 JS에게 getMovies 함수에게  
+    const {                                               // 조금 시간이 필요하고 우리는 그걸 기다려야한다 라는 것을 의미합니다.
+      data: {
+        data: { movies }
+      }
+
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+      );
+     this.setState({ movies, isLoading: false }); 
+  };
 
   componentDidMount() {
     this.getMovies();    
-  }
+  };
 
   render() {
-    const { isLoading } = this.state;
-     return  <div> { isLoading ? "Loading..." : "We are ready" } </div>
-        
-      
-    }
+    const { isLoading, movies } = this.state;
+     return (
+      <section className="container">  
+        { isLoading ? (
+        <div className='loader'>
+          <span className="loader__text">Loading...</span>
+        </div> 
+        ): ( 
+          <div className="movies">
+            {movies.map(movie => (
+            <Movie
+              key={movie.id} 
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+              /> 
+          ))}
+          </div>
+        )}
+        </section>
+     );
+  }
 }
+
+
 
 
  
